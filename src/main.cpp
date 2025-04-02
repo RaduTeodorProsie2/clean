69d153448cb683d289b9daf89d6d0c7721ee9c64
RaduTeodorProsie2 <abamon.game@gmail.com>
2025-04-02 12:18:03 +0300

    Update and rename main.cpp to src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 int main()
 {
     sf::Music instrumental;
-    const bool open = instrumental.openFromFile("Inst.ogg");
+    const bool open = instrumental.openFromFile("../assets/songs/Inst.ogg");
     
     if(!open) exit(1);
     std::cout << "All good!";
